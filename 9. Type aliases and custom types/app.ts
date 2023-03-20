16349849a3afc78d0c4db9aff593150ed8760c66
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-03-20 22:10:10 +0100

    function types and callbacks

--- 9. Type aliases and custom types/app.ts
+++ 9. Type aliases and custom types/app.ts
@@ -6,6 +6,13 @@ function printResult(num: number): void {
   console.log('Result: ' + num);
 }
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
+addAndHandle(5, 6, printResult);
+
 printResult(add(5, 12));
 
 let combineValues: (a: number, b: number) => number;
