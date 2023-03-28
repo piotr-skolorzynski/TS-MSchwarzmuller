91abf5386be6448a514a8aacf6e672d333bcd167
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-03-28 20:30:06 +0200

    unknown type

--- 9. Type aliases and custom types/app.ts
+++ 9. Type aliases and custom types/app.ts
@@ -1,22 +1,9 @@
-function add(n1: number, n2: number): number {
-  return n1 + n2;
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = 'Max';
+
+if (typeof userInput === 'string') {
+  userName = userInput;
 }
-
-function printResult(num: number): void {
-  console.log('Result: ' + num);
-}
-
-function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
-  const result = n1 + n2;
-  cb(result);
-}
-
-addAndHandle(5, 6, printResult);
-
-printResult(add(5, 12));
-
-let combineValues: (a: number, b: number) => number;
-
-combineValues = add;
-
-console.log(combineValues(8, 8));
