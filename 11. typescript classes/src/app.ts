3bd730ae22dfafb8452c57d0e5e89903a2286100
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-04-04 20:53:07 +0200

    private and public access modifiers

--- 11. typescript classes/src/app.ts
+++ 11. typescript classes/src/app.ts
@@ -1,5 +1,6 @@
 class Department {
   name: string;
+  private employees: string[] = []; //pozwala na dostęp do tej wartości tylko poprzez metodę w klasie
 
   constructor(n: string) {
     this.name = n;
@@ -8,8 +9,21 @@ class Department {
   describe() {
     console.log('Department: ' + this.name);
   }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
 const accounting = new Department('Accounting');
 
+accounting.addEmployee('Max');
+accounting.addEmployee('Skolo');
+
 accounting.describe(); //wywołanie metody na obiekcie
+accounting.printEmployeeInformation();
