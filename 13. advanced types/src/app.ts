3a79c40986110606c79efbc544d9028f7b91685c
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-04-11 12:57:39 +0200

    more on type guards

--- 13. advanced types/src/app.ts
+++ 13. advanced types/src/app.ts
@@ -8,7 +8,17 @@ type Employee = {
   startDate: Date;
 };
 
-type ElevatedEmployee = Admin & Employee;
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log('Name: ' + emp.name);
+  if ('privileges' in emp) {
+    console.log('Priviliges: ' + emp.privileges);
+  }
+  if ('startDate' in emp) {
+    console.log('Start Date: ' + emp.startDate);
+  }
+}
 
 const e1: ElevatedEmployee = {
   name: 'Piotr',
@@ -16,7 +26,51 @@ const e1: ElevatedEmployee = {
   startDate: new Date(),
 };
 
+printEmployeeInformation(e1);
+
+type ElevatedEmployee = Admin & Employee;
+
 type Combinable = string | number;
 type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
+
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === 'string' || typeof b === 'string') {
+    return a.toString() + b.toString();
+  }
+
+  return a + b;
+}
+
+class Car {
+  drive() {
+    console.log('Driving...');
+  }
+}
+
+class Truck {
+  drive() {
+    console.log('Driving a truck ...');
+  }
+
+  loadCargo(amount: number) {
+    console.log('Loading a cargo ...' + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
