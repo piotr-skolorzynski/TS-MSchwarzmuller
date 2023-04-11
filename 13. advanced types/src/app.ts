851b21fc02b9b2034230a0b93638f5883cfd2e47
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-04-11 15:22:43 +0200

    function overloads

--- 13. advanced types/src/app.ts
+++ 13. advanced types/src/app.ts
@@ -30,11 +30,14 @@ printEmployeeInformation(e1);
 
 type ElevatedEmployee = Admin & Employee;
 
-type Combinable = string | number;
 type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
 
+type Combinable = string | number;
+
+function add(a: number, b: number): number;
+function add(a: string, b: string): string;
 function add(a: Combinable, b: Combinable) {
   if (typeof a === 'string' || typeof b === 'string') {
     return a.toString() + b.toString();
@@ -42,6 +45,8 @@ function add(a: Combinable, b: Combinable) {
 
   return a + b;
 }
+
+const result = add('Max', ' Schwarz');
 
 class Car {
   drive() {
