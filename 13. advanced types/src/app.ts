e1754b4e48a1c2a1c3f536c7e10e93407188f098
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-04-11 13:13:13 +0200

    discriminated unions

--- 13. advanced types/src/app.ts
+++ 13. advanced types/src/app.ts
@@ -74,3 +74,32 @@ function useVehicle(vehicle: Vehicle) {
 
 useVehicle(v1);
 useVehicle(v2);
+
+interface Bird {
+  type: 'bird';
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type: 'horse';
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+  switch (animal.type) {
+    case 'bird':
+      console.log('flying with speed: ', animal.flyingSpeed);
+      break;
+
+    case 'horse':
+      console.log('running with speed: ', animal.runningSpeed);
+      break;
+
+    default:
+      console.log('moving...');
+  }
+}
+
+moveAnimal({ type: 'bird', flyingSpeed: 55 });
