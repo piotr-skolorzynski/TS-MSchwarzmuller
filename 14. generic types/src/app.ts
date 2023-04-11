d2a3430a60037644f00988ee0f12cea3fd5ec3da
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-04-11 21:02:42 +0200

    another generic function

--- 14. generic types/src/app.ts
+++ 14. generic types/src/app.ts
@@ -12,3 +12,20 @@ function merge<T extends object, U extends object>(objA: T, objB: U) {
 }
 
 const mergedObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = 'Got no value';
+  if (element.length === 1) {
+    descriptionText = 'Got 1 element.';
+  }
+  if (element.length > 1) {
+    descriptionText = 'Got ' + element.length + ' elements.';
+  }
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe('Hi there!'));
