b99e04e28391191d4e4ab04a0392d2a22cf43030
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-04-25 22:56:33 +0200

    finish drag & drop

--- 16. drag & drop project/src/app.ts
+++ 16. drag & drop project/src/app.ts
@@ -62,7 +62,18 @@ class ProjectState extends State<Project> {
       numOfPeople
     );
     this.projects.push(newProject);
+    this.updateListeners();
+  }
 
+  moveProject(projectId: string, newStatus: ProjectStatus) {
+    const project = this.projects.find((prj) => prj.id === projectId);
+    if (project && project.status !== newStatus) {
+      project.status = newStatus;
+      this.updateListeners();
+    }
+  }
+
+  private updateListeners() {
     for (const listenerFn of this.listeners) {
       listenerFn(this.projects.slice());
     }
@@ -249,7 +260,11 @@ class ProjectList
 
   @Autobind
   dropHandler(event: DragEvent): void {
-    console.log(event.dataTransfer!.getData('text/plain'));
+    const prjId = event.dataTransfer!.getData('text/plain');
+    projectState.moveProject(
+      prjId,
+      this.type === 'active' ? ProjectStatus.Active : ProjectStatus.Finished
+    );
   }
 
   @Autobind
