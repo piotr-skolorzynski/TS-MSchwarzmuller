81cf120e958b0a544b7b29c4cba2e4e4c475bd38
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-04-26 21:34:52 +0200

    problem with namespace imports

--- 16. drag & drop project/src/components/project-item.ts
+++ 16. drag & drop project/src/components/project-item.ts
@@ -1,3 +1,8 @@
+/// <reference path="base-component.ts" />
+/// <reference path="../decorators/autobind.ts" />
+/// <reference path="../models/project.ts" />
+/// <reference path="../models/drag-drop.ts" />
+
 namespace App {
   //Project Item class
   export class ProjectItem
