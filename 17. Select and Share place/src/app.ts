b806c04796bad1c732b3c59fd87dd86b40eb3d41
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-04-30 00:20:47 +0200

    get user input

--- 17. Select and Share place/src/app.ts
+++ 17. Select and Share place/src/app.ts
@@ -1,1 +1,9 @@
-// Code goes here!
+const form = document.querySelector('form')!;
+const adressInput = document.getElementById('address')! as HTMLInputElement;
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const enteredAdress = adressInput.value;
+}
+
+form.addEventListener('submit', searchAddressHandler);
